ecda0a7e38a2184d65349a5bca8f76ef1902ba13
KH312 <kimhenry312@gmail.com>
2021-04-30 13:41:20 -0400

    JP9-77 ch01_ydi01_hello 0h 19m

---
+++ Chapter_01/generated/ch01_ydi01_hello/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ch01_ydi01_hello"}];updateSearchResults();
---
+++ Chapter_01/generated/ch01_ydi01_hello/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"ch01_ydi01_hello","l":"Hello"}];updateSearchResults();
