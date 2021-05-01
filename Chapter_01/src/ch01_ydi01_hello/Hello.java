cecc5aa30c777d48266528b494358cd781610e76
KH312 <kimhenry312@gmail.com>
2021-05-01 03:15:45 -0400

    JP9-77 ch01_ydi01_hello 0h 0m

--- Chapter_01/src/ch01_ydi01_hello/Hello.java
+++ Chapter_01/src/ch01_ydi01_hello/Hello.java
@@ -17,6 +17,7 @@ public class Hello
 	{
 		public static void main(String[] args)
 			{
-				System.out.println("Hello World!");
+				System.out.println("Hello world!");
+				System.exit(0);
 			}
 	}
