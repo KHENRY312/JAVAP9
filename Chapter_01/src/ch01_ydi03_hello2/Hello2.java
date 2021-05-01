2dc1c799cf2504311fccf357b6dc0fa2b06a9299
KH312 <kimhenry312@gmail.com>
2021-05-01 03:53:09 -0400

    JP9-79 ch01_ydi03_Hello2 0h 0m

--- Chapter_01/src/ch01_ydi03_hello2/Hello2.java
+++ Chapter_01/src/ch01_ydi03_hello2/Hello2.java
@@ -18,6 +18,7 @@ public class Hello2
 		public static void main(String[] args)
 			{
 				System.out.println("Hello, world!");
+				System.exit(0);
 			}
 		/* This class demonstrates the use of the println()
 		method to print the message Hello, world! */
