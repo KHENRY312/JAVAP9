89d96e04414eb4819d215c3a37cd5535737b330f
KH312 <kimhenry312@gmail.com>
2021-05-03 06:11:37 -0400

    JP9-77 ch01_ydi01_hello 0h 0m

--- Chapter_01/src/ch01_ydi01_hello/Hello.java
+++ Chapter_01/src/ch01_ydi01_hello/Hello.java
@@ -1,23 +1,23 @@
+package ch01_ydi01_hello;
+
 /*
  * Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled or reproduced
  * for other uses than educational; and not to be used in lieu of performing your own assignments.
  */
 
-package ch01_ydi01_hello;
 /*
 Project:             JAVAP9
 Module:            Chapter_01
 Package:          ch01_ydi01_hello
 Class:               Hello
 Programmer:    Kimberly Henry
-Date:                 Fri, 30-Apr-2021
-Time:                1:07 PM
-*/
+Date:                 Sat, 01-May-2021
+Time:                6:06 AM
+x*/
 public class Hello
 	{
 		public static void main(String[] args)
 			{
 				System.out.println("Hello world!");
-				System.exit(0);
 			}
 	}
