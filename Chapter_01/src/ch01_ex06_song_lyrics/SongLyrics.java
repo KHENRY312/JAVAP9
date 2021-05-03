402fbe6ddd223cdadfb3dfdda95aa343ac6a2682
KH312 <kimhenry312@gmail.com>
2021-05-03 07:26:57 -0400

    JP9-81 ch01_ex06_SongLyrics 0h 25m

--- Chapter_01/src/ch01_ex06_song_lyrics/SongLyrics.java
+++ Chapter_01/src/ch01_ex06_song_lyrics/SongLyrics.java
@@ -4,25 +4,24 @@
  */
 
 package ch01_ex06_song_lyrics;
-/*
-Project:             JAVAP9
-Module:            Chapter_01
-Package:          ch01_ex06_song_lyrics
-Class:               SongLyrics
-Programmer:    Kimberly Henry
-Date:                 Fri, 30-Apr-2021
-Time:                11:12 PM
-*/
+/**
+ * Project:            JAVAP9
+ * Module:           Chapter_01
+ * Package:          ch01_ex06_song_lyrics
+ * Class:              SongLyrics
+ * Programmer:   Kimberly Henry
+ * Date:               Mon, 03-May-2021
+ */
 public class SongLyrics
-	{
-		public static void main(String[] args)
-			{
-				System.out.println("Each day through my window I watch as she passes by.");
-				System.out.println("I say to myself your such a lucky guy");
-				System.out.println("To have a girl like her is truly a dream come true.");
-				System.out.println("Out of all the guys in the world she she belongs to me.");
-				System.out.println("It was just my imagination running away with me.");
-				System.out.println("It was just my imagination once again, running away with me.");
-				System.exit(0);
-			}
-	}
+   {
+      public static void main(String[] arg)
+         {
+            System.out.println("Each day through my window I watch as she passes by.");
+            System.out.println("I say to myself your such a lucky guy.");
+            System.out.println("To have a girl like her is truly a dream come true.");
+            System.out.println("Out of all the guys in the world she she belongs to me.");
+            System.out.println("It was just my imagination running away with me.");
+            System.out.println("It was just my imagination once again, running away with me.");
+            System.exit(0);
+         }
+   }
