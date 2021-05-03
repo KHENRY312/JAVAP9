d713fedc7c11b34f6dc419da416b360bb2bf0037
KH312 <kimhenry312@gmail.com>
2021-05-03 02:38:11 -0400

    JP9-78 ch01_ydi02_HelloErrors 0h 13m

--- Chapter_01/src/ch01_ydi02_hello_errors/HelloErrors.java
+++ Chapter_01/src/ch01_ydi02_hello_errors/HelloErrors.java
@@ -4,19 +4,19 @@
  */
 
 package ch01_ydi02_hello_errors;
-/*
-Project:             JAVAP9
-Module:            Chapter_01
-Package:          ch01_ydi02_hello_errors
-Class:               HelloErrors
-Programmer:    Kimberly Henry
-Date:                 Fri, 30-Apr-2021
-Time:                1:49 PM
-*/public class HelloErrors
-	{
-		public static void main(String[] args)
-			{
-				 System.out.println("Hello");
-				 System.out.println("This is a test");
-			}
-	}
+/**
+ * Project:           JAVAP9
+ * Module:          Chapter_01
+ * Package:         ch01_ydi02_hello_errors
+ * Class:              HelloErrors
+ * Programmer:   Kimberly Henry
+ * Date:               Mon, 03-May-2021
+ */
+public class HelloErrors
+   {
+      public static void main(String[] arg)
+         {
+            System.out.println("Hello");
+            System.out.println("This is a test");
+         }
+   }
