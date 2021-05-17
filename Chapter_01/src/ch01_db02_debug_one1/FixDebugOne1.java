8ec113636a7eac8ea5858946bb1e8f094cc78a8c
KH312 <kimhenry312@gmail.com>
2021-05-17 04:32:34 -0400

    JP9-88 ch01_db01 DebugOne1 0h 0m

---
+++ Chapter_01/src/ch01_db02_debug_one1/FixDebugOne1.java
@@ -0,0 +1,10 @@
+package ch01_db02_debug_one1;
+public class FixDebugOne1
+{
+//This program displays a greeting
+
+public static void main(String[]args)
+      {
+      System.out.println("Hello");
+      }
+}
