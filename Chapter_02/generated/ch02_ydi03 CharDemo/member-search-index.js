e2e07c56114c8711d4cdf426d64e598ac55e4fca
KH312 <kimhenry312@gmail.com>
2021-06-11 09:17:08 -0400

    JP9-95 ch02_ydi03 CharDemo

---
+++ Chapter_02/generated/ch02_ydi03 CharDemo/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ch02_ydi03_char_demo","c":"CharDemo","l":"CharDemo()","u":"%3Cinit%3E()"},{"p":"ch02_ydi03_char_demo","c":"CharDemo","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ Chapter_02/src/ch02_ydi03_char_demo/CharDemo.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled or
+ * reproduced for other uses than educational; and not to be used in lieu of performing your own
+ *  assignments.
+ */
+
+package ch02_ydi03_char_demo;
+/**
+ * Project:            JP9
+ * Module:           Chapter_02
+ * Package:          ch02_ydi03_char_demo
+ * Class:              CharDemo
+ * Programmer:   Kimberly Henry
+ * Date:               Fri, 11-Jun-2021
+ */
+public class CharDemo
+   {
+      public static void main(String[] arg)
+         {
+            char initial = 'A';
+            System.out.println(initial);
+            System.out.print("\t\"abc\\def\bghi\n\njkl");
+         }
+   }
